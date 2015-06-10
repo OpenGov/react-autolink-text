ebd6bee32f668fcab29cbbb14c1a9011087e5af2
Joshua Roesslein <jroesslein@opengov.com>
2015-06-09 18:23:00 -0700

    Port over most of the parsing logic from Autolinker.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import matchParser from './match_parser';
+
 export default class AutoLinkText extends React.Component {
   render() {
-    return <span>Hello, world!</span>;
+    const {text} = this.props;
+    if (!text) {
+      return null;
+    }
+
+    const matches = matchParser(text);
+    const children = [];
+
+    let lastIndex = 0;
+    for (let m of matches) {
+      if (m.position.start !== 0) {
+        children.push(<span>{text.slice(lastIndex, m.position.start)}</span>);
+      }
+      children.push(<a href="#">{m.text}</a>);
+      lastIndex = m.position.end;
+    }
+
+    if (lastIndex < text.length) {
+      children.push(<span>{text.slice(lastIndex)}</span>);
+    }
+
+    return <span>{children}</span>;
   }
 }
+
+AutoLinkText.propTypes = {
+  text: React.PropTypes.string
+};
