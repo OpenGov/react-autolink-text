5f77dd6c04495d40f2ddd94123425aaf780771c1
Joshua Roesslein <jroesslein@opengov.com>
2015-06-12 02:37:18 -0700

    Implement more test coverage around auto linking and also allow truncating text (max length).

--- src/__tests__/index_test.js
+++ src/__tests__/index_test.js
@@ -1,12 +1,58 @@
 import React from 'react/addons';
 import AutoLinkText from '../index';
 
-const {renderIntoDocument, isElementOfType} = React.addons.TestUtils;
+const {renderIntoDocument, createRenderer} = React.addons.TestUtils;
 
 describe('<AutoLinkText />', function() {
-  it('renders a span', function() {
+  let renderer;
+
+  beforeEach(function() {
+    renderer = createRenderer();
+  });
+
+  function renderText(text, props={}) {
+    return React.renderToStaticMarkup(
+      <AutoLinkText text={text} {...props} />
+    );
+  }
+
+  it('renders an empty span if no text is provided', function() {
     expect(
-      React.findDOMNode(renderIntoDocument(<AutoLinkText />)).tagName
-    ).toBe('SPAN');
+      renderText()
+    ).toEqual('<span></span>');
+  });
+
+  describe('URL linking', function() {
+    it('should automatically link URLs in the form of http://opengov.com', function() {
+      expect(
+        renderText('Joe went to http://opengov.com')
+      ).toEqual('<span><span>Joe went to </span><a href="http://opengov.com">opengov.com</a></span>');
+    });
+
+    it('should automatically link localhost URLs when there is a protocol', function() {
+      expect(
+        renderText('Joe visited http://localhost today')
+      ).toEqual('<span><span>Joe visited </span><a href="http://localhost">localhost</a><span> today</span></span>');
+    });
+
+    it('should automatically link localhost URLs when there is a protocol and port', function() {
+      expect(
+        renderText('Joe visited http://localhost:8000 today')
+      ).toEqual('<span><span>Joe visited </span><a href="http://localhost:8000">localhost:8000</a><span> today</span></span>');
+    });
+
+    it('should automatically link URLs in the form of http://www.opengov.com (i.e. protocol and www prefix)', function() {
+      expect(
+        renderText('Joe checked out http://www.opengov.com last week')
+      ).toBe('<span><span>Joe checked out </span><a href="http://www.opengov.com">opengov.com</a><span> last week</span></span>');
+    });
+  });
+
+  describe('truncating text with links', function() {
+    it('should truncate a text span', function() {
+      expect(
+        renderText('Joe bookmarked http://opengov.com yesterday in his browser', {maxLength: 36})
+      ).toBe('<span><span>Joe bookmarked </span><a href="http://opengov.com">opengov.com</a><span> yesterday</span></span>');
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -4,29 +4,53 @@ import matchParser from './match_parser';
 
 export default class AutoLinkText extends React.Component {
   render() {
-    const {text} = this.props;
-    if (!text) {
-      return <span />;
-    }
-
-    const matches = matchParser(text);
-    const children = [];
-
-    let lastIndex = 0;
-    for (let m of matches) {
-      if (m.position.start !== 0) {
-        children.push(<span>{text.slice(lastIndex, m.position.start)}</span>);
-      }
-      children.push(<a href="#">{m.text}</a>);
-      lastIndex = m.position.end;
-    }
-
-    if (lastIndex < text.length) {
-      children.push(<span>{text.slice(lastIndex)}</span>);
-    }
-
-    return <span>{children}</span>;
+    const text = this.props.text || '';
+    return (
+      <span>{matchParser(text)::prepareElements(text)::truncate(this.props.maxLength)}</span>
+    );
   }
+}
+
+function prepareElements(text) {
+  let elements = [];
+  let lastIndex = 0;
+
+  for (const match of this) {
+    if (match.position.start !== 0) {
+      elements.push(<span>{text.slice(lastIndex, match.position.start)}</span>);
+    }
+    elements.push(<a href={match.text}>{match.text.replace('http://', '').replace('www.', '')}</a>);
+    lastIndex = match.position.end;
+  }
+
+  if (lastIndex < text.length) {
+    elements.push(<span>{text.slice(lastIndex)}</span>);
+  }
+
+  return elements;
+}
+
+function truncate(maxLength) {
+  if (!maxLength) return this;
+
+  let elements = [];
+  let length = 0;
+
+  for (const el of this) {
+    length += el.props.children.length;
+
+    if (length > maxLength) {
+      const truncatedText = el.props.children.slice(0, -(length - maxLength));
+      elements.push(
+        React.cloneElement(el, {}, truncatedText)
+      );
+      break;
+    }
+
+    elements.push(el);
+  }
+
+  return elements;
 }
 
 AutoLinkText.propTypes = {
