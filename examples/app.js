91ba51330dd529dbe0c1509f421351dd1a8fecee
Joshua Roesslein <jroesslein@opengov.com>
2015-06-12 10:58:01 -0700

    Update example to include a text area to input text.

--- examples/app.js
+++ examples/app.js
@@ -1,4 +1,24 @@
 import React from 'react';
 import AutoLinkText from 'react-autolink-text';
 
-React.render(<AutoLinkText />, document.getElementById('app'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  _onTextChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <AutoLinkText text={this.state.text} maxLength={10} /><br />
+        <textarea value={this.state.text} onChange={this._onTextChange.bind(this)} />
+      </div>
+    );
+  }
+}
+
+React.render(<App />, document.getElementById('app'));
